6c841464c8832990880512454c0d8066effc88a5
Cyber <cybergen49@gmail.com>
2023-03-24 05:45:01 -0500

    Add uptime

---
+++ main.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+const os = require('os');
+const clc = require('cli-color');
+const dayjs = require('dayjs');
+const diskUsage = require('diskusage');
+const utils = require('web-resources');
+
+const bar = (min, max, value) => {
+    const length = 30;
+    const percent = (value-min)/(max-min);
+    const filled = Math.floor(length*percent);
+    const empty = length-filled;
+    let fillColour = 'greenBright';
+    if (percent > 0.6) fillColour = 'yellowBright';
+    if (percent > 0.8) fillColour = 'redBright';
+    if (percent > 0.95) fillColour = 'red';
+    return clc.blackBright(`[${clc[fillColour]('|'.repeat(filled))}${' '.repeat(empty)}]`);
+};
+
+async function main() {
+    console.log(clc.whiteBright(`System Date & Time:`.padEnd(20)), clc.cyanBright(dayjs().format('MMMM D YYYY, h:mm:ss A')));
+    console.log(clc.whiteBright(`System uptime:`.padEnd(20)), clc.cyanBright(utils.formatSeconds(os.uptime())));
+    const memTotal = os.totalmem();
+    const memFree = os.freemem();
+    const memUsed = memTotal-memFree;
+    console.log(
+        clc.whiteBright(`Memory usage:`.padEnd(20)),
+        clc.white(utils.formatSize(memUsed).padStart(10)),
+        bar(0, memTotal, memUsed),
+        clc.white(utils.formatSize(memTotal))
+    );
+    const space = await diskUsage.check('/');
+    const spaceUsed = space.total-space.available;
+    console.log(
+        clc.whiteBright(`Disk usage:`.padEnd(20)),
+        clc.white(utils.formatSize(spaceUsed).padStart(10)),
+        bar(0, space.total, spaceUsed),
+        clc.white(utils.formatSize(space.total))
+    );
+}
+main();
