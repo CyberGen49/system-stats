e12c297210fa40295a57b38d48e2df0d2ce4a511
Cyber <cybergen49@gmail.com>
2024-03-19 19:09:15 -0500

    Add command options, networking, custom storage

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Install with `npm i -g cyber-system-stats`
 
 ## Usage
 Run `systemstats` to see the system time, uptime, memory, and disk usage.
+
+### Options
+* `--storage <path>`: Adds an additional storage meter for `<path>`. This option can be used multiple times.
+* `--padding <size>`: Sets the width of the label column to `<size>`, defaults to `24`.
+* `--barlength <size>`: Sets the width of bar meters to `<size>`, defaults to `30`.
+* `--separate`: Adds blank line separators between sections.
+* `--nopublicip`: Skips fetching the public IP from [ipify.org](https://www.ipify.org/).
+* `--dtformat <format>`: Sets the format of the date and time to `<format>`. Defaults to `YYYY-MM-DD H:mm:ss`. See [Day.js Format](https://day.js.org/docs/en/display/format) for details.
--- main.js
+++ main.js
@@ -4,13 +4,77 @@ const os = require('os');
 const clc = require('cli-color');
 const dayjs = require('dayjs');
 const diskUsage = require('diskusage');
-const utils = require('web-resources');
+const args = require('yargs').argv;
+
+// Compile arguments
+const storagePaths = [ '/' ];
+if (typeof args.storage === 'string') {
+    storagePaths.push(args.storage);
+} else if (typeof args.storage === 'object') {
+    storagePaths.push(...args.storage);
+}
+const paddingSize = args.padding || 24;
+const barLength = args.barlength || 30;
+
+// Compile network interfaces
+// https://stackoverflow.com/a/8440736
+const nets = os.networkInterfaces();
+const netInterfaces = [];
+for (const name of Object.keys(nets)) {
+    const result = {
+        name: name, addresses: []
+    };
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4;
+        if (net.family === familyV4Value && !net.internal) {
+            result.addresses.push(net.address);
+        }
+    }
+    if (result.addresses.length)
+        netInterfaces.push(result);
+}
+
+const roundSmart = (number, decimalPlaces = 0) => {
+    const factorOfTen = Math.pow(10, decimalPlaces);
+    return Math.round(number * factorOfTen) / factorOfTen;
+}
+
+const formatSize = (bytes) => {
+    if (bytes < 1000) return `${bytes} B`;
+    bytes /= 1024;
+    if (bytes < (1000)) return `${roundSmart(bytes, 0)} KB`;
+    bytes /= 1024;
+    if (bytes < (1000)) return `${roundSmart(bytes, 1)} MB`;
+    bytes /= 1024;
+    if (bytes < (1000)) return `${roundSmart(bytes, 2)} GB`;
+    bytes /= 1024;
+    if (bytes < (1000)) return `${roundSmart(bytes, 2)} TB`;
+    return "-";
+}
+
+const getRelativeDate = (target, anchor = Date.now()) => {
+    const isFuture = (anchor-target < 0) ? true : false;
+    let diff = Math.abs(anchor-target);
+    diff = Math.round(diff/1000);
+    if (diff < 120) // Less than 120 seconds
+        return (isFuture) ? `In a moment` : `Moments ago`;
+    diff = Math.round(diff/60);
+    if (diff < 120) // Less than 120 minutes
+        return (isFuture) ? `${diff} mins from now` : `${diff} mins ago`;
+    diff = Math.round(diff/60);
+    if (diff < 72) // Less than 72 hours
+        return (isFuture) ? `${diff} hours from now` : `${diff} hours ago`;
+    diff = Math.round(diff/24);
+    const days = diff;
+    return (isFuture) ? `${diff} days from now` : `${diff} days ago`;
+}
 
 const bar = (min, max, value) => {
-    const length = 30;
     const percent = (value-min)/(max-min);
-    const filled = Math.floor(length*percent);
-    const empty = length-filled;
+    const filled = Math.floor(barLength*percent);
+    const empty = barLength-filled;
     let fillColour = 'greenBright';
     if (percent > 0.6) fillColour = 'yellowBright';
     if (percent > 0.8) fillColour = 'redBright';
@@ -19,26 +83,46 @@ const bar = (min, max, value) => {
 };
 
 async function main() {
-    console.log(clc.whiteBright(`System Date & Time:`.padEnd(20)), clc.cyanBright(dayjs().format('MMMM D YYYY, h:mm:ss A')));
-    console.log(clc.whiteBright(`System uptime:`.padEnd(20)), clc.cyan(utils.getRelativeDate(Date.now()-(os.uptime()*1000)).replace(' ago', '')));
+    if (args.separate) console.log();
+    // Output date and time
+    console.log(clc.whiteBright(`System date and time:`.padEnd(paddingSize)), clc.cyanBright(dayjs().format('YYYY-MM-DD H:mm:ss')));
+    // Output uptime
+    console.log(clc.whiteBright(`System uptime:`.padEnd(paddingSize)), clc.cyan(getRelativeDate(Date.now()-(os.uptime()*1000)).replace(' ago', '')));
+    if (args.separate) console.log();
+    // Output memory usage
     const memTotal = os.totalmem();
     const memFree = os.freemem();
     const memUsed = memTotal-memFree;
     console.log(
-        clc.whiteBright(`Memory usage:`.padEnd(20)),
-        clc.white(utils.formatSize(memUsed).padStart(10)),
+        clc.whiteBright(`Memory usage:`.padEnd(paddingSize)),
+        clc.white(formatSize(memUsed).padStart(10)),
         bar(0, memTotal, memUsed),
-        clc.white(utils.formatSize(memFree).padEnd(10)),
-        clc.blackBright(`${utils.formatSize(memTotal)} total`)
+        clc.white(formatSize(memTotal).padEnd(10))
     );
-    const space = await diskUsage.check('/');
-    const spaceUsed = space.total-space.available;
-    console.log(
-        clc.whiteBright(`Disk usage:`.padEnd(20)),
-        clc.white(utils.formatSize(spaceUsed).padStart(10)),
-        bar(0, space.total, spaceUsed),
-        clc.white(utils.formatSize(space.available).padEnd(10)),
-        clc.blackBright(`${utils.formatSize(space.total)} total`)
-    );
+    // Output storage meters
+    for (const storagePath of storagePaths) {
+        const space = await diskUsage.check(storagePath);
+        const spaceUsed = space.total-space.available;
+        console.log(
+            clc.whiteBright(`Usage of ${storagePath}:`.padEnd(paddingSize)),
+            clc.white(formatSize(spaceUsed).padStart(10)),
+            bar(0, space.total, spaceUsed),
+            clc.white(formatSize(space.total).padEnd(10))
+        );
+    }
+    if (args.separate) console.log();
+    // Output network interfaces
+    for (const netInterface of netInterfaces) {
+        console.log(
+            clc.whiteBright(`Addresses of ${netInterface.name}:`.padEnd(paddingSize)),
+            clc.magentaBright(netInterface.addresses.join(', '))
+        );
+    }
+    // Output public IP
+    if (!args.nopublicip) {
+        const res = await (await fetch('https://api.ipify.org?format=json')).json();
+        console.log(clc.whiteBright(`Public IP:`.padEnd(paddingSize)), clc.magentaBright(res.ip));
+    }
+    if (args.separate) console.log();
 }
 main();
